d1a17669c149749038248659a49e8459400491d1
Pipelon3 <Pipelon999@gmail.com>
2019-06-18 16:17:23 -0600

    Sort of working

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,45 +1,6 @@
-//Stuff to make the game work
-
-function generateQuiz(questions, quizContainer, resultsContainer, submitButton){
-
-	function showQuestions(questions, quizContainer){
-        var output = [];
-        car answers;
-        for(car i=0; i<questions.length; i++){
-            answers = [];
-            for(letter in questions[i].answers){
-                answers.push(
-                    '<label>'
-                        +'<input type="radio" name="question'+i+'" value='letter+'"> '
-                        + letter + ':'
-                        + questions[i].answers[letter]
-                    + '</label>'
-                );
-            }
-            output.push(
-                '<div class="question">' + questions[i].question + '</div>'
-                +'<div class="answers">' + answers.join('') + '</div>'
-            );
-        }
-        quizContainer.innerHTML = output.join('');
-	}
-
-	function showResults(questions, quizContainer, resultsContainer){
-		
-	}
-
-	
-	showQuestions(questions, quizContainer);
-
-	
-	submitButton.onclick = function(){
-		showResults(questions, quizContainer, resultsContainer);
-	}
-}
-
 //Questions
 
-var myQuestions = [
+var questions = [
 	{
 		question: "Who is the Hero in the Legend of Zelda series?",
 		answers: {
@@ -59,7 +20,7 @@ var myQuestions = [
             d: 'Yoshi'
 		},
 		correctAnswer: 'c'
-    }
+    },
     {
 		question: "Pikachu belongs to which game series?",
 		answers: {
@@ -69,7 +30,7 @@ var myQuestions = [
             d: 'Fallout'
 		},
 		correctAnswer: 'a'
-    }
+    },
     {
 		question: "What city does GTA 5 take place in?",
 		answers: {
@@ -79,7 +40,7 @@ var myQuestions = [
             d: 'Los Santos'
 		},
 		correctAnswer: 'd'
-    }
+    },
     {
 		question: "What is the name of the protagonist in God of War?",
 		answers: {
@@ -91,3 +52,74 @@ var myQuestions = [
 		correctAnswer: 'c'
 	}
 ];
+
+//Timer
+
+var timer;
+
+var game = {
+  correct: 0,
+  incorrect: 0,
+  counter: 120,
+
+  countdown: function() {
+    game.counter--;
+    $("#counter-number").html(game.counter);
+    if (game.counter === 0) {
+      console.log("TIME UP");
+      game.finished();
+    }
+  },
+
+  start: function() {
+    timer = setInterval(game.countdown, 1000);
+
+    $("#sub-wrapper").prepend(
+      "<h2>Time Left: <span id='counter-number'>120</span> Seconds</h2>"
+    );
+
+    $("#start").remove();
+
+    for (var i = 0; i < questions.length; i++) {
+      card.append("<h2>" + questions[i].question + "</h2>");
+      for (var j = 0; j < questions[i].answers.length; j++) {
+        card.append("<input type='radio' name='question-" + i +
+          "' value='" + questions[i].answers[j] + "''>" + questions[i].answers[j]);
+      }
+	}
+
+	card.append("<button id='finish'>Finished</button>");
+  },
+
+  finished: function() {
+    var inputs = card.children("input:checked");
+    for (var i = 0; i < inputs.length; i++) {
+      if ($(inputs[i]).val() === questions[i].correctAnswer) {
+        game.correct++;
+      } else {
+        game.incorrect++;
+      }
+    }
+    this.result();
+  },
+
+  result: function() {
+    clearInterval(timer);
+
+    $("#sub-wrapper h2").remove();
+
+    card.html("<h2>Finished!</h2>");
+    card.append("<h3>Correct: " + this.correct + "</h3>");
+    card.append("<h3>Incorrect: " + this.incorrect + "</h3>");
+  }
+};
+
+//Stuff to make the game work
+
+$(document).on("click", "#start", function() {
+	game.start();
+  });
+  
+  $(document).on("click", "#finished", function() {
+	game.finished();
+  });
